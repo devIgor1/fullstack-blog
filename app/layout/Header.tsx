c6fb6296e650220f810b382e0a5c9167b996424d
devIgor1 <igormraesmm@gmail.com>
2024-02-22 13:58:56 -0300

    feat: add the right logo

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -10,9 +10,7 @@ const Header = () => {
     <SessionProvider>
       <div className="border-b border-black flex justify-between">
         <Link href="/" className="flex items-center">
-          <h1 className="text-4xl px-2 py-4 font-bricolageGrotesque">
-            Coffers
-          </h1>
+          <h1 className="text-4xl px-2 py-4">Coffers</h1>
           <Image src="/logo.png" width={100} height={100} alt="logo.png" />
         </Link>
         <Button />
