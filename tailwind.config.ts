f482e1dfc17ce30cf69532402c31f1d70db9910d
devIgor1 <igormraesmm@gmail.com>
2024-02-22 09:31:49 -0300

    feat: add custom font and sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container: {
+        center: true,
+        screens: {
+          sm: "600px",
+          md: "728px",
+          lg: "984px",
+          xl: "1240px",
+          "2xl": "1300px",
+        },
+      },
+      fontFamily: {
+        bricolageGrotesque: ["Bricolage Grotesque", "sans-serif"],
       },
       borderColor: {
         DEFAULT: "black",
