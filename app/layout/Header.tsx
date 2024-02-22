d66098aa8479d32a32e48035362c14dd5e1aa46f
devIgor1 <igormraesmm@gmail.com>
2024-02-22 09:32:43 -0300

    feat: initial header styling

---
+++ app/layout/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="border-b border-black flex justify-between">
+      <div className="text-4xl px-2 py-4">LOGO</div>
+      <button className="text-white bg-black p-4">Sign In</button>
+    </div>
+  )
+}
+
+export default Header
