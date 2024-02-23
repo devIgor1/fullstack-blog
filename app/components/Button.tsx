170810dfb55bea8fe8241cfcbcfdf97e54c728f2
devIgor1 <igormraesmm@gmail.com>
2024-02-23 10:25:45 -0300

    style: change button style

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,7 @@
 "use client"
-
+import { ImSpinner2 } from "react-icons/im"
 import { signIn, signOut, useSession } from "next-auth/react"
+import { CiLogout } from "react-icons/ci"
 
 const Button = () => {
   const { data: session, status } = useSession()
@@ -10,14 +11,31 @@ const Button = () => {
   }
 
   if (status === "loading") {
-    return <div>Loading...</div>
+    return (
+      <div className="text-white bg-black p-4 flex items-center justify-center">
+        <span className="animate-spin duration-300">
+          <ImSpinner2 size={20} />
+        </span>
+      </div>
+    )
   }
 
   if (session) {
-    return <button onClick={handleLogoutUser}>Sign Out</button>
+    return (
+      <div className="text-white bg-black p-4 flex items-center justify-center flex-col">
+        <h1>Hi, {session.user.name}</h1>
+        <button onClick={handleLogoutUser} className="mt-5">
+          <CiLogout size={25} />
+        </button>
+      </div>
+    )
   }
 
-  return <div onClick={() => signIn()}>Sign In</div>
+  return (
+    <button onClick={() => signIn()} className="text-white bg-black p-4">
+      Sign In
+    </button>
+  )
 }
 
 export default Button
