f51e677e0c5a99ff0a18b34c6327b0a7762c0161
devIgor1 <igormraesmm@gmail.com>
2024-02-23 13:54:03 -0300

    feat: include author to get his image

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,30 +1,42 @@
+import React from "react"
 import { prisma } from "@/lib/prisma"
+import Image from "next/image"
 
-const Page = async ({
+const page = async ({
   params,
 }: {
   params: {
-    id?: string
+    id: string
   }
 }) => {
   const post = await prisma.post.findUnique({
     where: {
       id: Number(params.id),
     },
+    include: {
+      author: true,
+    },
   })
-
   console.log(post)
 
   return (
-    <div>
+    <div className="container-sm mt-6">
       {post && (
         <>
-          <h1>{post.title}</h1>
-          <p>{post.content}</p>
+          <h1 className="text-5xl mb-5">{post.title}</h1>
+          <div className="flex items-center">
+            <p className="text-sm text-gray-500">By:</p>
+            <div
+              className="w-7 h-7 ml-3 mr-2 rounded-full bg-contain"
+              style={{ backgroundImage: `url(${post.author.image})` }}
+            ></div>
+            <p className="text-base text-gray-500">{post.author.name}</p>
+          </div>
+          <p className="mt-4 text-lg">{post.content}</p>
         </>
       )}
     </div>
   )
 }
 
-export default Page
+export default page
