92493893298982947ccf90ce07224f36de6a0dd9
devIgor1 <igormraesmm@gmail.com>
2024-02-24 13:20:05 -0300

    style: change style of postcard

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -1,19 +1,20 @@
+import React from "react"
 import type { Post } from "@prisma/client"
 import Link from "next/link"
 
-interface PostCardProps {
+type Props = {
   post: Post
   className?: string
 }
 
-const PostCard = ({ post, className }: PostCardProps) => {
+const PostCard = ({ post, className }: Props) => {
   return (
     <Link
       href={`/blog/${post.id}`}
-      className={`${className} p-4 border-2 shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)] hover:scale-105 duration-300`}
+      className={`${className} p-4 rounded border-2 line-clamp-7 shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)] hover:scale-105 duration-300`}
     >
-      <h3 className="text-2xl sm:text-3xl mb-3">{post.title}</h3>
-      <p>{post.content}</p>
+      <h3 className="text-2xl lg:text-3xl mb-3">{post.title}</h3>
+      <p className="line-clamp-6">{post.content}</p>
     </Link>
   )
 }
