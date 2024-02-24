4b04ea86f1a26dd77135a89c11e746c34d37e6db
devIgor1 <igormraesmm@gmail.com>
2024-02-24 13:19:55 -0300

    style: change style of specific post

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -20,19 +20,27 @@ const page = async ({
   console.log(post)
 
   return (
-    <div className="container-sm mt-6">
+    <div className="container border-l-2 border-r-2 border-b-2 border-black">
       {post && (
         <>
-          <h1 className="text-5xl mb-5">{post.title}</h1>
-          <div className="flex items-center">
-            <p className="text-sm text-gray-500">By:</p>
+          {post.imgURL && (
             <div
-              className="w-7 h-7 ml-3 mr-2 rounded-full bg-contain"
-              style={{ backgroundImage: `url(${post.author.image})` }}
+              className="h-[500px] bg-cover bg-center mb-5"
+              style={{ backgroundImage: `url(${post.imgURL})` }}
             ></div>
-            <p className="text-base text-gray-500">{post.author.name}</p>
+          )}
+          <div className="p-4">
+            <h1 className="text-5xl mb-5">{post.title}</h1>
+            <div className="flex items-center">
+              <p className="text-sm text-gray-500">By</p>
+              <div
+                className="w-5 h-5 ml-3 mr-2 rounded-full bg-contain"
+                style={{ backgroundImage: `url(${post.author.image})` }}
+              ></div>
+              <p className="text-sm text-gray-500">{post.author.name}</p>
+            </div>
+            <p className="mt-4">{post.content}</p>
           </div>
-          <p className="mt-4 text-lg">{post.content}</p>
         </>
       )}
     </div>
