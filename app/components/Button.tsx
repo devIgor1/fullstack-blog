9aa265c2bc5e426155d3c46c5dc063f52fbec932
devIgor1 <igormraesmm@gmail.com>
2024-02-24 13:17:48 -0300

    feat: finish header and custom button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,9 +1,10 @@
 "use client"
-import { ImSpinner2 } from "react-icons/im"
 import { signIn, signOut, useSession } from "next-auth/react"
+import Link from "next/link"
 import { CiLogout } from "react-icons/ci"
+import { ImSpinner2 } from "react-icons/im"
 
-const Button = () => {
+export const Button = () => {
   const { data: session, status } = useSession()
 
   const handleLogoutUser = () => {
@@ -22,20 +23,22 @@ const Button = () => {
 
   if (session) {
     return (
-      <div className="text-white bg-black p-4 flex items-center justify-center flex-col">
-        <h1>Hi, {session?.user?.name}</h1>
-        <button onClick={handleLogoutUser} className="mt-5">
-          <CiLogout size={25} />
-        </button>
+      <div className="flex items-stretch">
+        <Link href="/blog/new" className="mr-6 hover:underline self-center">
+          ✍️ Write a Post
+        </Link>
+        <div className="text-white bg-black p-4 flex items-center justify-center flex-col">
+          <h1>Hi, {session?.user?.name}</h1>
+          <button onClick={handleLogoutUser} className="mt-5">
+            <CiLogout size={25} />
+          </button>
+        </div>
       </div>
     )
   }
-
   return (
     <button onClick={() => signIn()} className="text-white bg-black p-4">
       Sign In
     </button>
   )
 }
-
-export default Button
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,14 +2,14 @@
 
 import Image from "next/image"
 import Link from "next/link"
-import Button from "./Button"
+import { Button } from "./Button"
 
 const Header = () => {
   return (
     <div className="border-b border-black flex justify-between">
-      <Link href="/" className="flex items-center">
-        <h1 className="text-4xl px-2 py-4">Coffers</h1>
-        <Image src="/logo.png" width={100} height={100} alt="logo.png" />
+      <Link href="/" className="flex items-center px-2">
+        <Image src="/logo.png" width={50} height={50} alt="logo.png" />
+        <h1 className="text-4xl px-2 py-4">Papers</h1>
       </Link>
       <Button />
     </div>
