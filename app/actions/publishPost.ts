7711ee1b2170a8be17501120756df80c689bbb57
devIgor1 <igormraesmm@gmail.com>
2024-02-27 14:38:40 -0300

    fix: add export default

--- app/actions/publishPost.ts
+++ app/actions/publishPost.ts
@@ -1,5 +1,5 @@
 "use server"
-import prisma from "@/lib/prisma"
+import { prisma } from "@/lib/prisma"
 import { Prisma } from "@prisma/client"
 
 export async function createPost(postInfo: Prisma.PostUncheckedCreateInput) {
