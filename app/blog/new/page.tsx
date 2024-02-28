3d8865ec696d6560f9599a7809164fae9f8057a0
devIgor1 <igormraesmm@gmail.com>
2024-02-28 16:24:10 -0300

    refactor: change auth method

--- app/blog/new/page.tsx
+++ app/blog/new/page.tsx
@@ -1,10 +1,9 @@
 import { getServerSession } from "next-auth"
 import NewBlogForm from "./components/NewBlogForm"
-import { authOptions } from "@/lib/auth"
 import { redirect } from "next/navigation"
 
 const page = async () => {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession()
 
   if (!session) {
     redirect("/")
